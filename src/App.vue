a9f19ecde8fe5318ab3b2437568178809bd91319
bootcamperu <rpabootcamperu@gmail.com>
2025-07-23 19:54:22 -0500

    Se adiciona un cambio

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue + Monolito - Tipo de Integracion Merge" />
+  <HelloWorld msg="Vite + Vue + Monolito - Tipo de Integracion Merge -Nunca trabajes en una rama de integracion DEVELOP" />
 </template>
 
 <style scoped>
